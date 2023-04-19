2e026f8079ca2f6bf89c9674be3fb99196b1c659
Dennis-Oliver Steiner <dsteiner@brockhaus-ag.de>
2023-04-19 21:28:22 +0200

    add classes and test

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,14 @@
+package guru.springframework;
+
+public class Dollar extends Money {
+
+  public Dollar(int amount) {
+    this.amount = amount;
+  }
+
+  public Dollar times(int multiplier) {
+    return new Dollar(amount * multiplier);
+  }
+
+
+}
---
+++ src/test/java/guru/springframework/MoneyTest.java
@@ -0,0 +1,42 @@
+package guru.springframework;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MoneyTest {
+
+  @Test
+  void testMultiplicationDollar() {
+    Dollar five = new Dollar(5);
+    Dollar product = five.times(2);
+    assertEquals(new Dollar(10), product);
+
+    product = five.times(3);
+    assertEquals(new Dollar(15), product);
+  }
+
+  @Test
+  void testEqualityDollar() {
+    assertEquals(new Dollar(5), new Dollar(5));
+    assertNotEquals(new Dollar(5), new Dollar(8));
+  }
+
+  @Test
+  void testMultiplicationFranc() {
+    Franc five = new Franc(5);
+    Franc product = five.times(2);
+    assertEquals(new Franc(10), product);
+
+    product = five.times(3);
+    assertEquals(new Franc(15), product);
+  }
+
+  @Test
+  void testEqualityFranc() {
+    assertEquals(new Franc(5), new Franc(5));
+    assertNotEquals(new Franc(5), new Franc(8));
+    assertNotEquals(new Dollar(5), new Franc(5));
+  }
+}
